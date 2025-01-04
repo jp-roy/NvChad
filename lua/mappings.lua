52b1728938a4173f5a6ce4719adf0fc33b57b01d
Jean-Philippe Roy <jproy84@gmail.com>
2025-01-04 09:10:11 +0100

    Extract custom mappings and options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,41 @@
 require "nvchad.mappings"
 
--- add yours here
-
+local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-----------------------------------------
+--        JP Roy config
+-----------------------------------------
+
+-- quickly exit insert with quick jk sequence
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map('n', '<leader>o', ':NvimTreeToggle<CR>', opts)
+
+map('n', '<leader>tn', ':TestNearest<CR>', opts)
+map('n', '<leader>tf', ':TestFile<CR>', opts)
+map('n', '<leader>tl', ':TestLast<CR>', opts)
+
+
+-- Auto indent pasted text
+map('n', 'p', "p=`]<C-o>''", opts)
+map('n', 'P', "P=`]<C-o>''", opts)
+
+-- Git blame
+map('n', '<leader>gb', ':Git blame<CR>', opts)
+
+-- Edit .init.lua
+map('n', '<leader>v', ':e ~/.config/nvim.init.lua<CR>', opts)
+
+-- Go to quickfix list if on window above
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+
+-----------------------------------------
+--        Lsp
+-----------------------------------------
+
+map('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+
+map('n', 'gd', vim.lsp.buf.definition, opts)
+map('n', 'gD', vim.lsp.buf.declaration, opts)
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,58 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.autoread = true
+o.backspace = { 'indent', 'eol', 'start' }
+o.belloff = 'all'
+o.clipboard = 'unnamed'
+o.cursorline = true
+o.history = 100
+o.laststatus = 2
+o.backup = false
+o.compatible = false
+o.showmode = false
+o.swapfile = false
+o.writebackup = false
+o.number = true
+o.numberwidth = 5
+o.relativenumber = true
+o.ruler = true
+o.showcmd = true
+o.showmatch = true
+o.ttimeoutlen = 0
+o.visualbell = true
+
+-- Search
+o.hlsearch = true
+o.incsearch = true
+o.wildmenu = true
+o.wildmode = 'full'
+
+-- Indentation
+o.autoindent = true
+o.expandtab = true
+o.smartindent = true
+o.smarttab = true
+o.shiftwidth = 2
+o.softtabstop = 2
+o.tabstop = 2
+
+-- Scrolling
+o.scrolloff = 8
+o.sidescrolloff = 15
+o.sidescroll = 1
+
+-----------------------------------------
+--        highlightedyank
+-----------------------------------------
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",  -- The highlight group to use
+      timeout = 150,          -- Duration in ms
+      on_visual = true        -- Only highlight if the yank was in visual mode
+    })
+  end,
+})
+
